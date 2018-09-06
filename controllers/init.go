fa45b7987496c05467fcfbf9a6dabf1747d8982e
付登龙 <1185694600@qq.com>
2018-09-07 00:24:40 +0800

    fix: cors

--- controllers/init.go
+++ controllers/init.go
@@ -2,13 +2,14 @@ package controllers
 
 import (
 	"github.com/gamelife1314/go-note/common"
+	"github.com/kataras/iris"
 	"github.com/kataras/iris/mvc"
 )
 
 func init() {
 
-	mvc.New(common.App.Party("/user")).Handle(new(UserController))
-	mvc.New(common.App.Party("/upload")).Handle(new(UploadController))
-	mvc.New(common.App.Party("/site")).Handle(new(SiteController))
-	mvc.New(common.App.Party("/topic")).Handle(new(TopicController))
+	mvc.New(common.App.Party("/user").AllowMethods(iris.MethodOptions)).Handle(new(UserController))
+	mvc.New(common.App.Party("/upload").AllowMethods(iris.MethodOptions)).Handle(new(UploadController))
+	mvc.New(common.App.Party("/site").AllowMethods(iris.MethodOptions)).Handle(new(SiteController))
+	mvc.New(common.App.Party("/topic").AllowMethods(iris.MethodOptions)).Handle(new(TopicController))
 }
